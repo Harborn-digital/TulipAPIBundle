0d71e2e8eb8966a6d8fa43d1f225c19d05e02d47
Niels Nijens <nijens.niels@gmail.com>
2016-04-12 16:47:34 +0200

    Add TulipAPIBundle class

---
+++ TulipAPIBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ConnectHolland\TulipAPIBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * TulipAPIBundle.
+ *
+ * @author Niels Nijens <niels@connectholland.nl>
+ */
+class TulipAPIBundle extends Bundle
+{
+}
